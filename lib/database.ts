beb66c6e5a4f1aca70b29496715fec9e1a108142
golddick <63362187+golddick@users.noreply.github.com>
2025-01-11 17:20:21 +0100

    d

--- lib/database.ts
+++ lib/database.ts
@@ -1,17 +1,22 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma: PrismaClient;
-}
- 
-let prisma : PrismaClient
- 
-if (process.env.NODE_ENV !== "production") prisma = new PrismaClient()
-else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma;
+  var prisma: PrismaClient;
 }
 
-export const database = prisma
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV !== "production") {
+  // In non-production environments, a new Prisma Client is created every time the module is imported
+  console.log("Creating new Prisma client instance in non-production environment");
+  prisma = new PrismaClient();
+} else {
+  // In production, use a global Prisma Client to avoid creating multiple instances
+  if (!global.prisma) {
+    console.log("Creating global Prisma client instance in production environment");
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export const database = prisma;
